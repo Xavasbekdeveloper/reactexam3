7db28f8fcc459a5b0c2cd1238db243b21d72cdd9
Xavasbekdeveloper <wolfcomp6@gmail.com>
2024-07-24 01:35:15 +0500

    up

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,55 @@
+import React, { memo } from "react";
+import banner1 from "../../assets/images/banner1.png";
+import banner2 from "../../assets/images/banner2.png";
+import banner3 from "../../assets/images/banner3.png";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+import "./banner.scss";
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <section className="banner">
+      <div className="container banner__container">
+        {/* 1 */}
+        <div className="banner__card">
+          <div className="banner__card__info">
+            <h3 className="banner__title">Living Room</h3>
+            <Link className="banner__link" to={"/shop"}>
+              Shop Now <FaArrowRightLong />
+            </Link>
+          </div>
+          <div className="banner__card__img">
+            <img src={banner1} alt="banner img" />
+          </div>
+        </div>
+        {/* 2 */}
+        <div className="banner__card">
+          <div className="banner__card__info">
+            <h3 className="banner__title">Bedroom</h3>
+            <Link className="banner__link" to={"/shop"}>
+              Shop Now <FaArrowRightLong />
+            </Link>
+          </div>
+          <div className="banner__card__img">
+            <img src={banner2} alt="banner img" />
+          </div>
+        </div>
+        {/* 3 */}
+        <div className="banner__card">
+          <div className="banner__card__info">
+            <h3 className="banner__title">Kitchen</h3>
+            <Link className="banner__link" to={"/shop"}>
+              Shop Now <FaArrowRightLong />
+            </Link>
+          </div>
+          <div className="banner__card__img">
+            <img src={banner3} alt="banner img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default memo(Banner);
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { memo } from "react";
 import "./home.scss";
 import Hero from "../../components/hero/Hero";
+import Banner from "../../components/banner/Banner";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Banner />
     </>
   );
 };
